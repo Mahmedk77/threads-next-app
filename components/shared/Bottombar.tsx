65cbf7d211baaad916b0f7a9e5f1350cffbcb6ae
Mahmedk77 <mak.212601@gmail.com>
2026-03-23 01:05:34 +0500

    debug profile route

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { sidebarLinks } from '@/constants';
+import { useAuth } from '@clerk/nextjs';
+import { getProxiedPluginState } from 'next/dist/build/build-context';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -10,19 +12,28 @@ const Bottombar = () => {
 
 
   const pathname = usePathname();
+  const {isLoaded, userId} = useAuth();
+
+  const getRoute = (route: string) => {
+    if (route === "/profile" && userId ) return  `/profile/${userId}`;
+    return route;
+  }
 
   return (
     <footer className='xl:hidden fixed bottom-0 w-full z-50 bg-black backdrop-blur-lg'>
         <div className='px-4 py-2 flex items-center gap-3 '>
           {
           sidebarLinks.map((element) => {
+
+            const resolvedRoute = getRoute(element.route)
             const isActive =
             // route = "/" | pathname = "/about" 
             //TODO Clear this concept again
-            (pathname.includes(element.route) && element.route.length > 1) ||
-            pathname === element.route;
+            (pathname.includes(resolvedRoute) && resolvedRoute.length > 1) ||
+            pathname === resolvedRoute;
+
             return(
-              <Link href={ element.route } key={ element.label } className={`p-2 flex flex-col justify-center items-center gap-2 w-full rounded-md ${ isActive && "bg-[#877EFF]"}`}>
+              <Link href={ resolvedRoute } key={ element.label } className={`p-2 flex flex-col justify-center items-center gap-2 w-full rounded-md ${ isActive && "bg-[#877EFF]"}`}>
                   <Image src={element.imgURL} alt='sidebar imgs' width={24} height={24} />
                   <p className='text-white text-sm md:text-lg font-medium max-sm:hidden'> { element.label.split(/\s+/)[0] } </p>
               </Link>
